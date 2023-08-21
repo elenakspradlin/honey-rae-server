ebab37fc467147c84516622a2ebe0ce7f5d04796
Elena Spradlin <ekspradlin@gmail.com>
2023-08-21 14:38:17 -0400

    Service Tickets Views Up and Running

--- honeyrae/urls.py
+++ honeyrae/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls import include
 from rest_framework import routers
-from repairsapi.views import CustomerView
+from repairsapi.views import CustomerView, EmployeeView, ServiceTicketView
 from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
 from repairsapi.views import register_user, login_user
 
 router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'serviceTickets', ServiceTicketView, 'serviceTicket')
 router.register(r'customers', CustomerView, 'customer')
+router.register(r'employees', EmployeeView, 'employee')
 urlpatterns = [
     path('', include(router.urls)),
     path('register', register_user),
